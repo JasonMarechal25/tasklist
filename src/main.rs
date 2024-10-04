4500f1cc0160221edad303e5f01b6b41ff3bab4b
Jason Marechal <jason.marechal@gmail.com>
2024-10-04 20:34:17 +0200

    Improve imutability

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::cell::Cell;
+
 fn main() {
     println!("Hello, world!");
 }
@@ -8,19 +10,18 @@ struct Task {
 }
 
 struct TaskRepository {
-    last_id: i32,
+    next_id: Cell<i32>,
 }
 
 impl Default for TaskRepository {
     fn default() -> Self {
-        TaskRepository { last_id: 0, }
+        TaskRepository { next_id: Cell::new(1), }
     }
 }
 
 impl TaskRepository {
-    fn new_task(&mut self, description: String) -> Task {
-        self.last_id += 1;
-        return Task { description: description, id: self.last_id };
+    fn new_task(&self, description: String) -> Task {
+        return Task { description: description, id: self.next_id.replace(self.next_id.get()+1) };
     }
 }
 
@@ -30,7 +31,7 @@ mod tests {
 
     #[test]
     fn task_added() {
-        let mut task_repository = TaskRepository::default();
+        let task_repository = TaskRepository::default();
         let task = task_repository.new_task(String::from("TestTask"));
         assert_eq!(task.description, "TestTask");
         assert_eq!(task.id, 1);
@@ -38,7 +39,7 @@ mod tests {
 
     #[test]
     fn task_id_incremental() {
-        let mut task_repository = TaskRepository::default();
+        let task_repository = TaskRepository::default();
         let task = task_repository.new_task(String::from("TestTask"));
         let task2 = task_repository.new_task(String::from("otherTask"));
         assert_eq!(task2.description, "otherTask");
